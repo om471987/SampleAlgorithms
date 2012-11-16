55c0dc499a954e7f520db406bece4860c1a93a77
omkar <omkar.panhalkar@live.com>
2012-11-15 21:44:45 -0500

    Created scaffold

---
+++ SampleAlgorithms.Tests/LinkedListTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SampleAlgorithms;
+
+namespace SampleAlgorithms.Tests
+{
+    [TestFixture]
+    public class LinkedListTests
+    {
+        [Test]
+        public void TestItTest()
+        {
+            Assert.IsTrue(LinkedList.TestIt());
+        }
+        
+    }
+}
---
+++ SampleAlgorithms/LinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAlgorithms
+{
+    public static class LinkedList
+    {
+        public static bool TestIt()
+        {
+            return true;
+        }
+    }
+}
